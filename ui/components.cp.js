f808c5d8c7b89ecf2848db8e637d0d5f64cfda2a
Linus Hagemann <mail@linushagemann.de>
2024-02-13 17:26:58 +0100

    Commited from within lively.next.

--- ui/components.cp.js
+++ ui/components.cp.js
@@ -1,1 +1,8 @@
-'format esm';
+import { component } from 'lively.morphic/components/core.js';
+import { pt } from 'lively.graphics/geometry-2d.js';
+import { Color } from 'lively.graphics/color.js';
+
+export const transitiveComponent = component({
+  extent: pt(100, 100),
+  fill: Color.red
+});
